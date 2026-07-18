4144930cd93df89ec29ee66cc63a6f08e5d81bb2
aashwinshukla <aashwinshukla585@gmail.com>
2026-07-18 17:10:11 +0530

    Enhance for.py by adding examples of iterating over a list of friends and improving comments for better understanding of for loop functionality. Included additional notes on using range with different start values.

--- Chapter-10-Loop/for.py
+++ Chapter-10-Loop/for.py
@@ -13,3 +13,20 @@ for index in range(10):
     print(index)
     
 # here 0 to 9 all number get printed
+# if you put range(3, 10) you will get 
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8 
+# 9 
+
+
+for index in range(len(friends)):
+    print(friends[index])
+    
+# all the above are mostly looping in an array
+
+# you can add if function or other inside of for function or vice versa make things work better
+
