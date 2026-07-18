1744a173dc8437770ae15e35d8c9448306995b8c
aashwinshukla <aashwinshukla585@gmail.com>
2026-07-18 17:49:44 +0530

    Refine error handling in tryExcept.py by specifying exceptions for ZeroDivisionError and ValueError. This improves the clarity of error messages and enhances the educational value of the code.

--- Chapter-13-Try-And-Except/tryExcept.py
+++ Chapter-13-Try-And-Except/tryExcept.py
@@ -11,6 +11,13 @@ try:
     print(number)
 except:
     print("invalid Input")
-          
+
 
 # the code above will try something and if doesnt make any sense then it will do what is in except 
+# since the except input is way to broad it will say invalid input for anything thats in the try 
+#thats why we can make it error specific as below
+
+except ZeroDivisionError:
+    print("Divided by zero")
+except ValueError:
+    print("invalid input")
