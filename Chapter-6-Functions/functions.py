2a140d8c2f433ea7d43d46174173516d3ca9197e
aashwinshukla <aashwinshukla585@gmail.com>
2026-07-18 13:52:21 +0530

    Add functions.py to define a simple function `say_hi()` that prints a greeting message. Included comments explaining function usage and the need to call the function for execution.

---
+++ Chapter-6-Functions/functions.py
@@ -0,0 +1,9 @@
+def say_hi():
+    print("hello User ")
+    
+# def gives program idea that we want to use function
+# after writing function name everything under it will be part of the function
+# we added print("Hello User") inside the function , if you run the code nothing will happen 
+#because you need to call the function first 
+
+say_hi()
