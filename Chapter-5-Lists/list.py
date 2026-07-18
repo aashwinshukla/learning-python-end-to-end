6476a429b1a67aaa2a163dd5224c9797bca156ea
aashwinshukla <aashwinshukla585@gmail.com>
2026-07-18 13:13:50 +0530

    Add indexing examples to list.py, demonstrating how to access list elements using positive and negative indices, and include descriptive comments for clarity.

--- Chapter-5-Lists/list.py
+++ Chapter-5-Lists/list.py
@@ -3,3 +3,9 @@ friends = ["Kevin", "Karen", "Jim"]
 #you can also put number or True/False
 
 print(friends)
+
+
+print(friend[0])
+#we can also use indexing to call out the values inside list 
+#0 will return kevin 
+#we can put -1 inside friend[] and we will get Jim
