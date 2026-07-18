5232e4791a495a2e0bb0049795480623470c3a56
aashwinshukla <aashwinshukla585@gmail.com>
2026-07-18 12:08:07 +0530

    Add additional string manipulation examples in working_with_string.py, including string length, indexing, and case sensitivity explanations.

--- Strings/working_with_string.py
+++ Strings/working_with_string.py
@@ -27,4 +27,19 @@ print(phrase.isupper())
 #using isupper() will give bool value after checking wether the phrase is completely upper case or not
 print(phrase.upper().isupper())
 #this will run the first case to make everything upper and then run second case to give true as answer
-  
+
+
+print(len(phrase))
+#this will give us the length of the string
+
+print(phrase[0])
+#this will give us the capital H from the string
+#[ ] helps to call out specific alphabet or character of the string 
+#indexing starts from 0
+
+print(phrase.index("w"))
+#here we give the program a parameter or value to find from the variable phrase 
+#if inside "" i put capital H it will return 0 
+#if inside "" i put Wor then it will return 7 which is the starting index of Wor
+#this is case sensitive if you put lower case w it will not return 7 
+
