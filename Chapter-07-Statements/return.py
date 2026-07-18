b6e89a667daf49bc1aa5d3c242a15732a6f3cae6
aashwinshukla <aashwinshukla585@gmail.com>
2026-07-18 14:09:44 +0530

    Add return.py to define a function `cube(num)` that calculates the cube of a number. Included a print statement for demonstration and comments explaining the use of the return statement and variable storage for output.

---
+++ Chapter-07-Statements/return.py
@@ -0,0 +1,9 @@
+def cube(num):
+    return num*num*num
+
+print(cube(3))
+
+# as you can see we used return statement because we wanted an output from the function outside of the function
+# we can also store cube(3) in result variable outside of the function 
+# and then print it out 
+# you cants write anything after the return statement in the function
