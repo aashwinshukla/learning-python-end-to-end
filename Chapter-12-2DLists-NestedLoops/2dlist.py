c71ca1e24ab04eef5d0475ee9632ef049bdddf21
aashwinshukla <aashwinshukla585@gmail.com>
2026-07-18 17:25:26 +0530

    Add 2dlist.py to demonstrate a 2D list structure in Python. Includes a sample grid and a comment explaining how to access elements, enhancing educational content on nested lists.

---
+++ Chapter-12-2DLists-NestedLoops/2dlist.py
@@ -0,0 +1,11 @@
+number_grid = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8 ,9],
+    [0]
+    
+]
+#a normal list 
+
+print(number_grid[0][0])
+#this is 2d list where we are accessing 0,0 element that is 1 from the list similar to 2d array from c
