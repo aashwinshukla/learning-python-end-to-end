b2fe29ac95200ad3d87d1dfebf49e633bf40ce46
aashwinshukla <aashwinshukla585@gmail.com>
2026-07-18 11:51:17 +0530

    Refactor variable-and-data-type.py to improve clarity and organization of data type explanations

---
+++ Strings/working_with_string.py
@@ -0,0 +1,13 @@
+print("hello world")
+#normal string and print statement
+
+print("hello\"world")
+#here \ helped the program to know that what ever comes after the \ it should be printed as well
+#hello"world
+
+#otherwise
+print("hello"world")
+#here the program thinks string ended after hello because of " and no \"
+
+
+
