b4fc6cbc4c5cfd732ba1fe43043e7d8c05a2a692
aashwinshukla <aashwinshukla585@gmail.com>
2026-07-18 17:02:39 +0530

    Add .vscode/settings.json for Python REPL configuration and update for.py to include additional examples of for loops, demonstrating iteration over a list of friends. Enhanced comments for clarity on the code's functionality.

--- Chapter-10-Loop/for.py
+++ Chapter-10-Loop/for.py
@@ -1,2 +1,11 @@
 for letter in "Python":
     print(letter)
+    
+# here for loop is printing out all the letters of Python on different line 
+
+friends = ["jim", "luna", "steve"]
+for friend in friends:
+    print(friend)
+    
+#here each name of friend is getting printed on different lines
+
