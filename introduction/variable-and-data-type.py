a0fdc81d292034830c5246af69083faa58f912d5
aashwinshukla <aashwinshukla585@gmail.com>
2026-07-18 11:41:20 +0530

    Enhance variable-and-data-type.py with additional comments on data types and formatting corrections

--- introduction/variable-and-data-type.py
+++ introduction/variable-and-data-type.py
@@ -1,11 +1,15 @@
+#anything in python saved in "" are called strings
+#when storing numbers there is no need to use quotation mark
+
+
 character_name = "mark" 
 #character_name is the variable here which hold the value in ""
 
-character_age = "26"
+character_age = "26"  #or just 26 
 #character_age in the variable here which hold the value in ""
 
 print("there was once a man name "+ character_name +".")
-print("he was "+ character_age +"years old") 
+print("he was "+ character_age +" years old") 
 #adding +variable+ inside the print will add the value of the variable automatically
 
 character_name = "joe"
@@ -15,5 +19,5 @@ print("there was a man named "+character_name+".")
 
 #result
 #there was once a man name mark.
-#he was 26years old
+#he was 26 years old
 #there was a man named joe.
