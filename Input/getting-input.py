41aa4c39e743ac11ddea64226d0ad9bc6458866e
aashwinshukla <aashwinshukla585@gmail.com>
2026-07-18 12:36:47 +0530

    Add getting-input.py to demonstrate user input handling in Python, including capturing and displaying name and age.

---
+++ Input/getting-input.py
@@ -0,0 +1,9 @@
+name = input("Enter your name: ")
+#here input() helps to put value, which was taken from the user , inside the variable name 
+
+age = input("Enter your age: ")
+#here input() helps to put value, which was taken from the user , inside the variable age
+
+print("hello "+name+ "!")
+print("you are "+age+ " year old")
+#at last we can print the input out
