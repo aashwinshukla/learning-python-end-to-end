9489fe478fff31665291704ed62eb8d4955fed91
aashwinshukla <aashwinshukla585@gmail.com>
2026-07-18 12:00:57 +0530

    Add working_with_string.py to demonstrate string manipulation techniques in Python, including concatenation and case conversion methods.

--- Strings/working_with_string.py
+++ Strings/working_with_string.py
@@ -6,8 +6,25 @@ print("hello\"world")
 #hello"world
 
 #otherwise
-print("hello"world")
+#print("hello"world")
 #here the program thinks string ended after hello because of " and no \"
 
 
+phrase = "Hello World"
+#here variable has a string 
+print(phrase)
+#this will print the phrase
 
+print(phrase + " nice to meet you ")
+#we can do concatenation where variable will be connected with the extra sting we have written inside the print
+
+print(phrase.lower())
+#everything will be printed in lower case
+print(phrase.upper())
+#everything will be printed in upper case
+
+print(phrase.isupper())
+#using isupper() will give bool value after checking wether the phrase is completely upper case or not
+print(phrase.upper().isupper())
+#this will run the first case to make everything upper and then run second case to give true as answer
+  
