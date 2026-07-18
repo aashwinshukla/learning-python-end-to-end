1f94438182c947e9858de7711331fd07dc9d1a5f
aashwinshukla <aashwinshukla585@gmail.com>
2026-07-18 12:58:05 +0530

    Remove basic-calculator.py as it is no longer needed; future enhancements will be implemented in a different module.

---
+++ 4. Building/mad-libs.py
@@ -0,0 +1,10 @@
+colour = input("Enter a colour:")
+plural_noun = input("Enter a Plural noun:")
+celebrity = input("Enter a name of a Celebrity:")
+
+
+print("Roses are "+colour)
+print(plural_noun+ " are Blue")
+print("I love "+celebrity)
+
+#you can add more input and print statement to make it better 
