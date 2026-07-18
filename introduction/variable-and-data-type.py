a1cfd5d192d93642d38174e54ae7ede91c8c6ce8
aashwinshukla <aashwinshukla585@gmail.com>
2026-07-18 11:33:01 +0530

    Add variable-and-data-type.py to demonstrate variable assignment and usage in Python

---
+++ introduction/variable-and-data-type.py
@@ -0,0 +1,19 @@
+character_name = "mark" 
+#character_name is the variable here which hold the value in ""
+
+character_age = "26"
+#character_age in the variable here which hold the value in ""
+
+print("there was once a man name "+ character_name +".")
+print("he was "+ character_age +"years old") 
+#adding +variable+ inside the print will add the value of the variable automatically
+
+character_name = "joe"
+#you can write the same variable again in the code and give it a new value and it will print the same from down here
+
+print("there was a man named "+character_name+".")  
+
+#result
+#there was once a man name mark.
+#he was 26years old
+#there was a man named joe.
