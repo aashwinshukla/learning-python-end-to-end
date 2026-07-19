ad9e50b9979f06edd3116ba99a77806013a33072
aashwinshukla <aashwinshukla585@gmail.com>
2026-07-19 19:07:42 +0530

    Add new module.py file to demonstrate the use of Python modules. This includes examples of how to print available modules and specific information about the math module, enhancing educational content on module usage in Python.

---
+++ 00-Module/module.py
@@ -0,0 +1,10 @@
+# module is a file containing code you want to include in your program use import to include a module 
+
+print(help("modules"))
+
+# above will print out all the modules on the terminal that are available 
+
+print(help("math"))
+
+#this will tell what is in maths (which is a module )
+
