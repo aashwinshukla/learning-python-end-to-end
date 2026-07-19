3efd44e7e82d556d9c3927a548752eb81b62bd8b
aashwinshukla <aashwinshukla585@gmail.com>
2026-07-19 14:35:25 +0530

    Add formatting examples in formate-specifier.py to demonstrate various string formatting techniques in Python. Include explanations for zero-padding, justification, and comma separation for improved educational content on number representation.

--- Chapter-03-Input/formate-specifier.py
+++ Chapter-03-Input/formate-specifier.py
@@ -9,3 +9,24 @@ print(f"price is {price:10}")
 # :10 this will add 10 spaces in the print including the price 
 #result = price is 3.14
 #         price is    3.14243
+
+print(f"price is {price:010}")
+# :010 we added 0 before 10 that will make all the spaces in previous result to show as zero 
+
+print(f"price is {price:<10}")
+# when used < the price will be left justified 
+print(f"price is {price:>10}")
+# when used > the price will be right justified or default as :10
+print(f"price is {price:^10}")
+#when used ^ the price is centered 
+
+print(f"price is {price:+}")
+# when used + the positive number will have a + infront of them 
+print(f"price is {price: }")
+# when left empty after : the price will have a single gap infront 
+print(f"price is {price:,}")
+# using ,  we can create a thousand place comma 
+#we can also add
+print(f"price is {price:,.2f}")
+#this we will do two things at the sme time ie thousand place comma and two decimal place round off
+
