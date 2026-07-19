405c4603aae901aa8c915ec342477b92163b7c96
aashwinshukla <aashwinshukla585@gmail.com>
2026-07-19 14:29:00 +0530

    Refactor working_with_string.py to include additional examples of string methods, such as .replace() and .split(). Enhance educational content on string manipulation in Python with clear explanations and use cases.

---
+++ Chapter-03-Input/formate-specifier.py
@@ -0,0 +1,11 @@
+# formate for print()
+
+price = 3.14243
+
+print(f"price is {price:.2f}")
+# :.2f this will round decimal to 2 places
+
+print(f"price is {price:10}")
+# :10 this will add 10 spaces in the print including the price 
+#result = price is 3.14
+#         price is    3.14243
