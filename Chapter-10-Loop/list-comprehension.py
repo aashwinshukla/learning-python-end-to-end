6ed27b3f7cf91dfa61d8c904cd0bc00e07b268ff
aashwinshukla <aashwinshukla585@gmail.com>
2026-07-19 18:49:37 +0530

    Add examples of list comprehension with conditions in list-comprehension.py to demonstrate filtering of positive and negative numbers. This enhancement enriches educational content by showcasing practical applications of list comprehensions in Python.

--- Chapter-10-Loop/list-comprehension.py
+++ Chapter-10-Loop/list-comprehension.py
@@ -26,3 +26,16 @@ print(fruits)
 
 fruits = [fruit.upper() for fruit in ["apple", "orange", "coconut", "pineapple"] ]
 print(fruits)
+
+
+# we are yet to see conditions so lets get to them 
+
+number = [1, -2, 3, -4, 5, -6]
+posi_num = [num for num in number if num>=0]
+neg_num = [num for num in number if num<0]
+
+
+print(posi_num)
+print(neg_num)
+#result = [1, 3, 5]
+#         [-2, -4, -6]
