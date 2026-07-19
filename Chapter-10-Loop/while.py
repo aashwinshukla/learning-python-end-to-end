a7801e3425d1281a5dc03eefa58562c10c2b1bb7
aashwinshukla <aashwinshukla585@gmail.com>
2026-07-19 14:46:43 +0530

    Add input validation example in while.py to ensure user provides a name. Implement a loop that prompts for input until a valid name is entered, enhancing educational content on user interaction and input handling in Python.

--- Chapter-10-Loop/while.py
+++ Chapter-10-Loop/while.py
@@ -6,3 +6,12 @@ while i<=10:
     i += 1
     
 print("done with loop")
+
+
+# below code shows how we can use loop to force someone to give input 
+name = input("Enter your name")
+while name == "":
+    print("You did not enter your name ")
+    name = input("Enter your name")
+
+print(f"Hello {name}!")
