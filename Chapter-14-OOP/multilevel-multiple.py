2c44da9e87c1d6700aabaa2d0a79b425fbc70f48
aashwinshukla <aashwinshukla585@gmail.com>
2026-07-20 16:17:08 +0530

    Refactor multilevel-multiple.py to introduce an Animal class, enhancing the inheritance structure for Prey and Predator classes. Added eat method to demonstrate multilevel inheritance, allowing Rabbit, Hawk, and Fish to utilize this method. Updated comments for clarity on inheritance concepts.

--- Chapter-14-OOP/multilevel-multiple.py
+++ Chapter-14-OOP/multilevel-multiple.py
@@ -1,11 +1,17 @@
 # Multiple Inheritance = inheritance from more than one parent class C(A, B)
 # Multilevel inheritance = inherit from a parent which inherits from another parent C(B) <- B(A) <- A
 
-class Prey: 
+
+class Animal: 
+    def eat(self): 
+        print(" this animal  is eating")
+        
+        
+class Prey(Animal): 
     def flee(self): 
         print("this animal is fleeing")
 
-class Predator:
+class Predator(Animal):
     def hunt(self): 
         print("this animal is hunting")
 
@@ -30,3 +36,14 @@ fish.hunt()
 
 # here is we put .flee with hawk and .hunt with rabbit it will not work since those attributes do not belong to them whereas fish can do both 
 # this is multiple inheritance
+
+# now to show multi level i added animal class with def eat(self)
+# and gave it to prey and predator 
+# now the following code should work because of multilevel inheritance
+
+rabbit.eat()
+hawk.eat()
+fish.eat()
+
+#this is like getting inheritance from parents that belonged to grandparents 
+
