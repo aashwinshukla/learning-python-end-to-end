6d82dbd1ad74d4400c4315324f8fae1832b49637
aashwinshukla <aashwinshukla585@gmail.com>
2026-07-20 14:48:14 +0530

    Refactor decryption logic in encryption program to enhance clarity and accuracy. Adjusted variable assignments for plain_text and cipher_text, and refined the decryption loop to ensure correct message output using the key.

---
+++ Chapter-14-OOP/object-and-class.py
@@ -0,0 +1,6 @@
+# object = a "bundle" of related attributes (variable) and methods (functions)
+#          ex. phone, cup, book
+#          you need a "class" to create many objects
+
+# class = (blueprint) used to design the structure and layout of an object 
+
