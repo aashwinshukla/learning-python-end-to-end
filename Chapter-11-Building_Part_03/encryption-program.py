cbed600554898c2c10e40524d7b58043a12a94bf
aashwinshukla <aashwinshukla585@gmail.com>
2026-07-20 13:26:18 +0530

    Fix decryption logic in encryption program by swapping variable assignments for plain_text and cipher_text. Updated the loop to correctly decrypt the message using the key, ensuring accurate output for both original and encrypted messages.

--- Chapter-11-Building_Part_03/encryption-program.py
+++ Chapter-11-Building_Part_03/encryption-program.py
@@ -29,13 +29,14 @@ print(f"original message : {plain_text}")
 print(f"encrypted message: {cipher_text}")
 
 #DECRYPTION
-plain_text = input("Enter a message to encrypt: ")
-cipher_text = ""
 
-for letter in plain_text:
-    index = chars.index(letter)
-    cipher_text += key[index]
+cipher_text= input("Enter a message to encrypt: ")
+plain_text = ""
+
+for letter in cipher_text:
+    index = key.index(letter)
+    plain_text += chars[index]
     
-print(f"original message : {plain_text}")
-print(f"encrypted message: {cipher_text}")
+print(f"original message : {cipher_text}")
+print(f"encrypted message: {plain_text}")
 
