be48aaa1f2b5457b15e51c74a9b40e45849138a4
aashwinshukla <aashwinshukla585@gmail.com>
2026-07-20 17:00:52 +0530

    Add duck typing example in polimorphism.py, introducing Animal, Dog, Cat, and Car classes to demonstrate polymorphic behavior through method overriding. Included a sample usage to illustrate the concept and updated comments for clarity on class interactions.

--- Chapter-14-OOP/polimorphism.py
+++ Chapter-14-OOP/polimorphism.py
@@ -36,3 +36,34 @@ for shape in shapes:
     print(f"{shape.area()} cm square")
     
 # now here our pizza is also a pizza, circle and a shape
+
+# now "Duck typing"
+
+class Animal:
+    alive = True
+
+class Dog(Animal):
+    def speak(self): 
+        print("woof")
+        
+class Cat(Animal):
+    def speak(self):
+        print("meow")
+        
+class Car:
+    alive = False
+    
+    def speak(self):
+        print("honk")
+    
+        
+animals = [Dog(), Cat(), Car()]
+
+for animal in animals:
+    animal.speak()
+    print(animal.alive)
+
+# we will get a error because car doesnt  have the speak attribute 
+
+# but we can change .horn to .speak in the class Car 
+#and then the code will run 
