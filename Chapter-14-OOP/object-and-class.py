55b828c962877069e34ca46756c532d9bf0619bc
aashwinshukla <aashwinshukla585@gmail.com>
2026-07-20 15:03:56 +0530

    Add Car class in carClass.py and update object-and-class.py to demonstrate object instantiation and attribute access. This change enhances the educational content by illustrating basic OOP principles in Python.

--- Chapter-14-OOP/object-and-class.py
+++ Chapter-14-OOP/object-and-class.py
@@ -4,3 +4,30 @@
 
 # class = (blueprint) used to design the structure and layout of an object 
 
+from carClass import Car
+
+
+# class Car: 
+#    def __init__(self, model, year, color, for_sale):
+#        self.model = model 
+#        self.year = year 
+#        self.color = color
+#        self.for_sale = for_sale
+        
+car1 = Car("mustang", 2024, "red", False)
+car2 = Car("Ferrari", 2004, "red", True)
+car3 = Car("Charger", 2026, "yellow", True)
+
+print(car1)
+# this will give us memory address of the Car object but we want to get specific attribute from it 
+
+print(car1.model)
+# the period ( . ) known as attribute access operator 
+print(car2.year)
+print(car3.color)
+print(car1.for_sale)
+
+# now if change car1 to car 2 or 3 in print statement is will print their specific attributes 
+
+# now we have commented the Car class and made a new file called carClass.py
+# and we will import the file at top 
